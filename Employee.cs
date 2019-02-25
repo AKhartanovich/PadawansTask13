de4599a25a1d2ba413a5de002d43efac9829e91b
Alex <khartanovichao@gmail.com>
2019-02-25 14:46:54 +0300

    123

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        public string _Surname { get; set; }
+        private int _Age { get; }
+        public Employee()
+        {
+            _Surname = string.Empty;
+        }
+        public Employee(string Surname, int Age)
+        {
+            _Surname = Surname;
+            _Age = Age;
+        }
+        private string GetAge()
+        {
+            return _Age.ToString();
+        }
+        public void ChangeSurname(string newSurname)
+        {
+            _Surname = newSurname;
+        }
+
+        public string GetInfo
+        {
+            get { return "Фамилия: {Employee.Surname}, Возраст: {Employee.Age}";  }
+        }
+    }
+}
